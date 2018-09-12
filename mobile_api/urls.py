243749170e9021197ce0d56646ad44d23c5c5268
Chamath Sandaru <sandaruchamath@gmail.com>
2018-09-12 18:12:42 +0530

    register shipping agent dummy api

--- mobile_api/urls.py
+++ mobile_api/urls.py
@@ -1,4 +1,4 @@
-from .views import VesselViewSet, VesselArrivalViewSet
+from .views import VesselViewSet, VesselArrivalViewSet, ShippingAgentRegister
 from rest_framework.routers import DefaultRouter
 from django.conf.urls import include, url
 
@@ -7,9 +7,11 @@ from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 router = DefaultRouter()
 router.register(r'vessels', VesselViewSet, base_name='vessel')
 router.register(r'vessel-arrivals', VesselArrivalViewSet, base_name='vessel_arrival')
+router.register
 
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^auth/token', obtain_jwt_token),
     url(r'^auth/token/refresh', refresh_jwt_token),
+    url(r'^users/sa/register', ShippingAgentRegister.as_view(), name='shiping_agent_register')
 ]
--- mobile_api/views.py
+++ mobile_api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
 from shipping_line.models import Vessel, VesselArrival
 from .serializers import VesselSerializer, VesselArrivalSerializer
-
+from rest_framework import status
+from rest_framework.response import Response
 
 class VesselViewSet(ModelViewSet):
     """
@@ -17,3 +19,13 @@ class VesselArrivalViewSet(ModelViewSet):
     """
     serializer_class = VesselArrivalSerializer
     queryset = VesselArrival.objects.all()
+
+
+
+class ShippingAgentRegister(APIView):
+    """
+    A model viewset for viewing and editing Vessel Arrival instances.
+    """
+
+    def post(self, request, format=None):
+        return Response({'msg': 'Successfully registered.'})
