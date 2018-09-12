2ddb9c05feffa542b32093008e944ea1c7184db9
Yashod <yashodgayashan@gamil.com>
2018-09-12 08:19:01 +0530

    add the add ship form and add notification button to the berth planner to chech what are the new ships which are unchecked

--- shipping_line/forms.py
+++ shipping_line/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm  
-from .models import ShippingLine,VesselArrival
+from .models import ShippingLine, VesselArrival,Vessel
 
 
 class VesselArrivalDetailsForm(ModelForm):
@@ -11,6 +11,15 @@ class VesselArrivalDetailsForm(ModelForm):
             'third_confirm',
             'modified_time',
             'created_time'
+        ]
+
+class VesselDetailsForm(ModelForm):
+    class Meta:
+        model = Vessel
+        fields = [
+            'vessel_name',
+            'vessel_loa',
+            'vessel_status'
         ]
 
 
