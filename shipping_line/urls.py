62d6b24c441b5118fab704bd461d05624bc62125
Yashod <yashodgayashan@gamil.com>
2019-01-24 14:12:50 +0530

    add temporary file to check

--- shipping_line/urls.py
+++ shipping_line/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import url, include
-from .views import vessel_listview,berth_schedule,view_history,remove_arrival,edit_arrival
+from .views import (
+    vessel_listview,
+    berth_schedule,
+    view_history,
+    remove_arrival,
+    edit_arrival,
+    vessel_timestamp
+)
 
 urlpatterns = [
     url('^$', vessel_listview, name="index"), 
+    url('^vessel-timestamp/$', vessel_timestamp, name="vessel_timestamp"),
     url('^berth-schedule/$', berth_schedule, name="berthSchedule"),
     url('^history/$', view_history, name="viewHistory"),
     url('^remove_items/(?P<item_id>[0-9]+)/$', remove_arrival, name="remove_arrival"),
